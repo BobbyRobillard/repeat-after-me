6f36b52951b9ebd654c6e801a483622ae6ef09f7
Bobby Robillard <robillard123@gmail.com>
2019-07-13 16:31:19 -0700

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -7,12 +7,14 @@ from tkinter import messagebox
 
 top = tk.Tk()
 
+
 def get_mouse_location():
     mouse = Controller()
     msg = messagebox.showinfo(
         "Hello Python",
         "The current pointer position is {0}".format(mouse.position)
     )
+
 
 button = tk.Button(top, text="Hello", command=get_mouse_location)
 button.pack()
