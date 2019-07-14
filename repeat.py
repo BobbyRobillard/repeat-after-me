d156b694b91758772871f6f3a2f721781164b1d0
Bobby Robillard <robillard123@gmail.com>
2019-07-13 23:43:41 -0700

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -1,4 +1,5 @@
 from pynput.mouse import Button, Controller
+from pynput import mouse
 
 import tkinter as tk
 
@@ -7,16 +8,33 @@ from tkinter import messagebox
 
 top = tk.Tk()
 
+action = []
+
+controller = Controller()
+
 
 def get_mouse_location():
-    mouse = Controller()
-    msg = messagebox.showinfo(
-        "Hello Python",
-        "The current pointer position is {0}".format(mouse.position)
-    )
+    return controller.position
 
 
-button = tk.Button(top, text="Hello", command=get_mouse_location)
+def add_mouse_location():
+    action.append(get_mouse_location())
+
+
+def on_click(x, y, button, pressed):
+    add_mouse_location()
+
+
+def take_action():
+    for move in action:
+        controller.position = (move[0], move[1])
+        controller.click(Button.left, 1)
+
+
+listener = mouse.Listener(on_click=on_click)
+listener.start()
+
+button = tk.Button(top, text="Hello", command=take_action)
 button.pack()
 
 top.mainloop()
