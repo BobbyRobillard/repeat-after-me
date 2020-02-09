fd0b1da9b745a00d44a282b87f33511a5c3d35e1
Bobby Robillard <robillard123@gmail.com>
2020-02-08 20:39:37 -0500

    More GUI Stuff

--- app.py
+++ app.py
@@ -6,17 +6,34 @@ from utils import get_current_profile
 import sys
 
 
+def on_click():
+    AddProfileWindow()
+
+
+class AddProfileWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(400, 200, 500, 500)
+        self.show()
+
+
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
         labelA = QLabel(self)
+        labelA.setGeometry(10, 10, 200, 50)
+        button = QPushButton("+", self)
+        button.setGeometry(150, 10, 50, 50)
+        button.clicked.connect(on_click)
 
         current_profile_name = get_current_profile("test")[2]
-        print(str(current_profile_name))
         labelA.setText(current_profile_name)
-        self.setGeometry(100, 100, 300, 200)
+        self.setGeometry(100, 50, 500, 500)
         self.show()
+
+
+
 
 
 App = QApplication(sys.argv)
