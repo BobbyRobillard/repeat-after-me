fb7dde3a94683634619f473444cea63493d86ed5
Bobby Robillard <robillard123@gmail.com>
2020-02-11 19:19:49 -0500

    Graphics and Functionality
    1) Navbar Added
    2) Help Window Now Opens

---
+++ HelpScreen.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'OtherWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class HelpWindow(object):
+    def setupUi(self, OtherWindow):
+        OtherWindow.setObjectName("OtherWindow")
+        OtherWindow.resize(200, 200)
+
+
+# if __name__ == "__main__":
+#     import sys
+#     app = QtWidgets.QApplication(sys.argv)
+#     OtherWindow = QtWidgets.QMainWindow()
+#     ui = Ui_OtherWindow()
+#     ui.setupUi(OtherWindow)
+#     OtherWindow.show()
+#     sys.exit(app.exec_())
--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from PyQt5.QtWidgets import *
 
 from utils import get_current_profile, add_profile, get_profiles
 
+from graphics import setup_navbar
+
+from HelpScreen import HelpWindow
+
 import sys
 
 
@@ -14,16 +18,19 @@ class Window(QMainWindow):
         super().__init__()
 
         self.labelA = QLabel(self)
-        self.labelA.setGeometry(10, 10, 200, 30)
+        self.labelA.setGeometry(10, 60, 200, 30)
 
         self.textbox = QLineEdit(self)
-        self.textbox.setGeometry(10, 60, 240, 30)
+        self.textbox.setGeometry(10, 120, 240, 30)
+
+        setup_navbar(self)
+
         button = QPushButton("+", self)
-        button.setGeometry(260, 60, 30, 30)
+        button.setGeometry(260, 120, 30, 30)
         button.clicked.connect(self.add_profile_btn_click)
 
         self.labelB = QLabel(self)
-        self.labelB.setGeometry(10, 100, 200, 100)
+        self.labelB.setGeometry(10, 135, 200, 100)
 
         current_profile_name = get_current_profile(username)[2]
         self.labelA.setText("CURRENT PROFILE:\n " + current_profile_name)
@@ -38,7 +45,20 @@ class Window(QMainWindow):
         self.labelA.setText("Added profile: {0}".format(profile_name))
         self.labelB.setText("YOUR PROFILES:\n" + get_profiles(1))
 
+    def open_help(self):
+        self.window = QMainWindow()
+        self.ui = HelpWindow()
+        self.window.show()
 
-App = QApplication(sys.argv)
-window = Window()
-sys.exit(App.exec())
+    def open_dashboard(self):
+        print("Opening Dashboard")
+
+    def start_quick_recording(self):
+        print("Opening Quick Record")
+
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    window = Window()
+    sys.exit(app.exec())
