c58bc2afe5a7cd1a4a9a06a80dbfb49723b264ee
Bobby Robillard <robillard123@gmail.com>
2020-04-08 13:16:07 -0400

    Fixed all bugs with interactive tutorial

--- website/static/website/js/main.js
+++ website/static/website/js/main.js
@@ -1,6 +1,39 @@
 var admin_email = "admin@techandmech.com";
 //------------------------------------------------------------------------------
+// For items that direct a user somewhere. I.e: trashcan (delete), pencil (edit)
+//------------------------------------------------------------------------------
 $('body').on('click', '.action-link', function(e) {
     e.stopPropagation();
     window.location.href = $(this).attr('href');
 });
+//------------------------------------------------------------------------------
+// For items that direct a user to the "save a recording" page
+//------------------------------------------------------------------------------
+$('.save').click(function() {
+  window.location.href = "{% url 'macros:save_recording' %}";
+});
+//------------------------------------------------------------------------------
+// Real Time Updates Bar
+//------------------------------------------------------------------------------
+const playModeSocket = new WebSocket(
+    'ws://'
+    + 'localhost:8000'
+    + '/ws/macros/'
+    + 'updates/'
+);
+
+playModeSocket.onmessage = function(e) {
+    const data = JSON.parse(e.data);
+    $("#play-mode").prop('checked', data.playMode);
+    $("#recording").prop('checked', data.isRecording);
+
+};
+
+playModeSocket.onclose = function(e) {
+    console.error('Chat socket closed unexpectedly');
+};
+
+setInterval(function() {
+  playModeSocket.send(JSON.stringify({}));
+}, 250);
+//------------------------------------------------------------------------------
